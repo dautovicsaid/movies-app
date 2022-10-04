a9c20557a57aecd2a058a53af96cd85449c1822b
dautovicsaid <83250294+dautovicsaid@users.noreply.github.com>
2022-10-04 08:53:10 +0200

    Homework final version

--- src/components/MoviesStatistics.jsx
+++ src/components/MoviesStatistics.jsx
@@ -3,26 +3,28 @@ import {allMovies} from "../constants/movies";
 import MovieCard from "./MovieCard";
 
 const MoviesStatistics = () => {
+    const [allMoviesCount,setAllMoviesCount] = useState();
     const [targetGenreCount, setTargetGenreCount] = useState();
     const [matchingYearCount, setMatchingYearCount] = useState();
     const [matchingYearRangeList, setMatchingYearRangeList] = useState([]);
     const [matchingGenreAndYearList, setMatchingGenreAndYearList] = useState([]);
-    const [matchingYearLastMovieName, setMatchingYearLastMovieName] = useState();
+    const [matchingYearLastMovie, setMatchingYearLastMovie] = useState();
+    var matchingYearMovies = allMovies.filter(movie => movie.year === 2010);
     const [targetActorMovies, setTargetActorMovies] = useState([]);
 
-
     useEffect(() => {
+        setAllMoviesCount(allMovies.length);
         setTargetGenreCount(allMovies.filter(movie => movie.genre === "Comedy").length);
         setMatchingYearCount(allMovies.filter(movie => movie.year === 2012).length);
         setMatchingYearRangeList(allMovies.filter(movie => movie.year >= 1990 && movie.year <= 1999));
         setMatchingGenreAndYearList(allMovies.filter(movie => movie.genre === "Drama" && movie.year > 2000));
-        setMatchingYearLastMovieName(allMovies.filter(movie => movie.year === 2010).pop().name);
+        setMatchingYearLastMovie(matchingYearMovies[matchingYearMovies.length-1]);
         setTargetActorMovies(allMovies.filter(movie => movie.actors.includes("Leonardo DiCaprio")));
     }, []);
 
     return (
         <div className="col-8 offset-2">
-            <p>Ukupan broj filmova: {allMovies.length}</p>
+            <p>Ukupan broj filmova: {allMoviesCount}</p>
             <p>Broj filmova sa žanrom "Komedija": {targetGenreCount}</p>
             <p>Broj filmova iz 2012 godine: {matchingYearCount}</p>
             <p>Lista filmova izmedju 1990 i 1999 godine:</p>
@@ -44,9 +46,19 @@ const MoviesStatistics = () => {
                 )}
             </ul>
             <p>
-                Poslednji film u listi iz 2010: {matchingYearLastMovieName}
+                Poslednji film u listi iz 2010:
             </p>
-            <p>Lista cover slika filmova u kojima glumi Leonardo DiCaprio:</p>
+            <div className="row row-cols-md-3 justify-content-center">
+            {matchingYearLastMovie && <MovieCard
+                name={matchingYearLastMovie.name}
+                year={matchingYearLastMovie.year}
+                genre={matchingYearLastMovie.genre}
+                actors={matchingYearLastMovie.actors}
+                coverImage={matchingYearLastMovie.coverImage}
+
+            />}
+            </div>
+            <p className="my-3">Lista cover slika filmova u kojima glumi Leonardo DiCaprio:</p>
             <ul>
                 <div className="row row-cols-3">
                     {targetActorMovies.map(movie => {
